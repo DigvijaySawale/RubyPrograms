46349ec709cd78e745fc2cae5422b98d6fcace1d
digvijay <sawaledigvijay70@gmail.com>
2025-11-12 16:13:33 +0000

    ruby hello world

---
+++ main.rb
@@ -0,0 +1,21 @@
+# puts "Hello World!" #returns nil
+
+# p "Hello World!"  #return string itself
+
+# print "Hello World!"  #no new line
+
+greeting = "Hello Ruby"
+puts greeting
+
+
+def say_hello
+  puts "Hello"
+end
+
+def say_hi(name) 
+  puts "Hi #{name}"
+end
+
+say_hello
+say_hi("Ram")
+say_hi "Sham"
